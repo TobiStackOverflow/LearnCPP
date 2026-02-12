883d1d8b863e87994e4cfa3cfebfdec88d541ce0
TobiStackOverflow <Schultze.Tobias@ebus-elektro.de>
2026-02-12 19:36:57 +0100

    test

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*
+*@autor: T.Schultze,        V1.0, 12.02.2026
+*
+*Changes:dhh
+*
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}   
